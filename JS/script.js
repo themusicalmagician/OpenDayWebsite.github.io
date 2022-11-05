84931a40ac79faeb6ea40db29a45a2332ae48b84
Mike Connotte <m.connotte@sintlucasedu.nl>
2022-11-05 14:04:14 +0100

    update

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+function playTriForce()
+{
+    const music1 = new Audio ('./Assets/soundfiles/BOTW.mp3');
+    music1.play();
+}
+
+function playMorshu()
+{
+    const music2 = new Audio ('./Assets/soundfiles/Morshu.mp3');
+    music2.play()
+}
+
+function playMoai()
+{
+    const music3 = new Audio ('./Assets/soundfiles/Moai.mp3');
+    music3.play();
+}
